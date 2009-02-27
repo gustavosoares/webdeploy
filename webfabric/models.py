7c0d6a2aa1ae6d281186921e2ed9b5cec66827b3
Gustavo Soares <gustavo@matrix.(none)>
2009-02-27 10:43:26 -0300

    remocao dos pyc e alteracao do models

--- webfabric/models.py
+++ webfabric/models.py
@@ -6,20 +6,31 @@ class Configuration(models.Model):
 	value = models.CharField(max_length=255)
 	fabfile = models.TextField()
 
+    def __unicode__(self):
+        return u'%s' % (self.name)
+
 
 class Template(models.Model):
 	name = models.CharField(max_length=100)
 	configuration = models.ForeignKey(Configuration)
+
+    def __unicode__(self):
+        return u'%s' % (self.name)
 
 class Project(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=200)
 	creation_dt = models.DateTimeField()
 	template = models.ForeignKey(Template)
+	
 #	environment = models.ManyToManyField(Environment)
+    def __unicode__(self):
+        return u'%s' % (self.name)
 
 class Environment(models.Model):
 	name = models.CharField(max_length=100)
 	hosts = models.CharField(max_length=500)
 	project = models.ForeignKey(Project)
 
+    def __unicode__(self):
+        return u'%s' % (self.name)
