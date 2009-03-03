fa77830af25d2c1ebff2e220cba0d2cf3cd9e571
Gustavo Soares <gustavo@matrix.(none)>
2009-03-03 17:13:22 -0300

    forms

--- webfabric/forms.py
+++ webfabric/forms.py
@@ -1,14 +1,17 @@
 from django import forms
 from deploy.admin.widgets import AdminDateWidget
 from deploy.admin.widgets import AdminTimeWidget
-
+from deploy.webfabric.models import Template
 #FORMS
-TEMPLATE_CHOICE = ( 
-		('none', '-----------------'),
-	)
+
 class ProjectForm(forms.Form):
     name = forms.CharField(initial="Nome do projeto")
     description = forms.CharField()
     creation_date = forms.CharField(widget=AdminDateWidget())
     creation_time = forms.CharField(widget=AdminTimeWidget())
-    Template = forms.ChoiceField(choices=TEMPLATE_CHOICE)
+    #retorna array com tuplas
+    CHOICES_LIST = [('0', '----------')]
+    template_list = Template.objects.all().values_list()
+    for l in template_list:
+        CHOICES_LIST.append(l)
+    template = forms.ChoiceField(choices=CHOICES_LIST)
